c1208849f9d1cbb6604a23e9cef92f060bfac0bc
trevorforrey <tforrey@asu.edu>
2017-02-15 23:59:33 +0000

    current working code. THANK YOU

--- try2.c
+++ try2.c
@@ -12,9 +12,9 @@
 
 #define ENABLE_BUTTON 4
 #define DIRECTION_BUTTON 5
-#define ENABLE_PIN 27
-#define CONTROL_PIN_1 28
-#define CONTROL_PIN_2 29
+#define ENABLE_PIN 2
+#define CONTROL_PIN_1 0
+#define CONTROL_PIN_2 1
 
 
 //////// VARIABLES //////////
@@ -62,42 +62,74 @@ int main (void) {
   pinMode (CONTROL_PIN_1, OUTPUT);
   pinMode (CONTROL_PIN_2, OUTPUT);
 
+  digitalWrite(ENABLE_PIN, LOW);
+  digitalWrite(CONTROL_PIN_1, LOW);
+  digitalWrite(CONTROL_PIN_2, LOW);
+
   // Setting up turning on and off DC motor function as interrupt 
   wiringPiISR (ENABLE_BUTTON, INT_EDGE_FALLING, &turnOnOffMotor);
 
   // Setting up switching direction of DC motor function as interrupt
   wiringPiISR (DIRECTION_BUTTON, INT_EDGE_FALLING, &switchMotorDirection);
 
-  while (1) {
-    digitalWrite(ENABLE_PIN, HIGH);
-    digitalWrite(CONTROL_PIN_1, HIGH);
-    digitalWrite(CONTROL_PIN_2, LOW);
-    printf("NOOOOOOOOOO\n"); delay (500);
-  }
+
+  //while (1) {
+
+    //printf("OFF\n");
+    //digitalWrite(ENABLE_PIN, LOW);
+    //digitalWrite(CONTROL_PIN_1, HIGH);
+    //digitalWrite(CONTROL_PIN_2, LOW);
+    //delay(1000);
+
+    
+    //printf("STILL OFF\n");
+    //digitalWrite(ENABLE_PIN, LOW);
+    //digitalWrite(CONTROL_PIN_1, LOW);
+    //digitalWrite(CONTROL_PIN_2, HIGH);
+    //delay(1000);
+
+    //printf("ON\n");
+    //digitalWrite(ENABLE_PIN, HIGH);
+    //digitalWrite(CONTROL_PIN_1, HIGH);
+    //digitalWrite(CONTROL_PIN_2, LOW);
+    //delay(1000);
+    
+
+    //digitalWrite(ENABLE_PIN, LOW);
+    //delay(1000);
+    
+    //printf("STILL ON\n");
+    //digitalWrite(ENABLE_PIN, HIGH);
+    //digitalWrite(CONTROL_PIN_1, LOW);
+    //digitalWrite(CONTROL_PIN_2, HIGH);
+    //delay(1000);
+  
+  //}
+
 
   while (1) {
     
     if (motorEnabled == 0) {
       // turn off motor
-      printf("OFF\n"); delay (500);
+      printf("OFF\n"); delay (1000);
       digitalWrite(ENABLE_PIN, LOW);
     } else if (motorEnabled == 1) {
       // turn on motor
-      printf("ON\n"); delay (500);
+      printf("ON\n"); delay (1000);
       digitalWrite(ENABLE_PIN, HIGH);
     }
     
     if (motorDirection == 0) {
       // turn direction A
-      printf("A\n"); delay (500);
+      printf("A\n"); delay (1000);
       digitalWrite(CONTROL_PIN_1, HIGH);
       digitalWrite(CONTROL_PIN_2, LOW);
     } else if (motorDirection == 1) {
       // turn direction B
-      printf("BBBBBBB\n"); delay (500);
+      printf("BBBBBBB\n"); delay (1000);
       digitalWrite(CONTROL_PIN_1, LOW);
       digitalWrite(CONTROL_PIN_2, HIGH);
     }
+    
   }
-
 }
