6866b4f0e78a23dc3adba3697884ceec8d7a5719
trevorforrey <tforrey@asu.edu>
2017-02-17 05:39:40 +0000

    try 2 is finished

--- try2.c
+++ try2.c
@@ -1,10 +1,5 @@
 #include <wiringPi.h>
-
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
-
 #include <signal.h>
 #include <softPwm.h>
 
@@ -31,21 +26,11 @@ volatile unsigned long interruptTime;
 ///////// FUNCTIONS //////////
 
 void turnOnOffMotor() {
-  //interruptTime = millis();
-  //if (interruptTime - lastInterruptTime < 100) {
-    motorEnabled = !motorEnabled;
-  //}
-  //lastInterruptTime = interruptTime;
-  //printf("enable finish\n");
+  motorEnabled = !motorEnabled;
 }
 
 void switchMotorDirection() {
-  //interruptTime = millis();
-  //if (interruptTime - lastInterruptTime < 100) {
-    motorDirection = !motorDirection;
-  //}
-  //lastInterruptTime = interruptTime; 
-  //printf("direction finish\n");
+  motorDirection = !motorDirection;
 }
 
 
@@ -75,62 +60,38 @@ int main (void) {
   // Set up ENABLE_PIN as a software pwm-able pin
   softPwmCreate(ENABLE_PIN,0,100);
 
-  //while (1) {
-
-    //printf("OFF\n");
-    //digitalWrite(ENABLE_PIN, LOW);
-    //digitalWrite(CONTROL_PIN_1, HIGH);
-    //digitalWrite(CONTROL_PIN_2, LOW);
-    //delay(1000);
-
-    
-    //printf("STILL OFF\n");
-    //digitalWrite(ENABLE_PIN, LOW);
-    //digitalWrite(CONTROL_PIN_1, LOW);
-    //digitalWrite(CONTROL_PIN_2, HIGH);
-    //delay(1000);
-
-    //printf("ON\n");
-    //digitalWrite(ENABLE_PIN, HIGH);
-    //digitalWrite(CONTROL_PIN_1, HIGH);
-    //digitalWrite(CONTROL_PIN_2, LOW);
-    //delay(1000);
-    
-
-    //digitalWrite(ENABLE_PIN, LOW);
-    //delay(1000);
-    
-    //printf("STILL ON\n");
-    //digitalWrite(ENABLE_PIN, HIGH);
-    //digitalWrite(CONTROL_PIN_1, LOW);
-    //digitalWrite(CONTROL_PIN_2, HIGH);
-    //delay(1000);
-  
-  //}
-
 
   while (1) {
     
     if (motorEnabled == 0) {
+
       // turn off motor
       printf("OFF\n"); delay (1000);
-      //digitalWrite(ENABLE_PIN, LOW);
+
       softPwmWrite(ENABLE_PIN,0);
+
     } else if (motorEnabled == 1) {
+
       // turn on motor
       printf("ON\n"); delay (1000);
-      //digitalWrite(ENABLE_PIN, HIGH);
+
       softPwmWrite(ENABLE_PIN, 60);
+
     }
     
     if (motorDirection == 0) {
+
       // turn direction A
       printf("A\n"); delay (1000);
+
       digitalWrite(CONTROL_PIN_1, HIGH);
       digitalWrite(CONTROL_PIN_2, LOW);
+
     } else if (motorDirection == 1) {
+
       // turn direction B
       printf("BBBBBBB\n"); delay (1000);
+
       digitalWrite(CONTROL_PIN_1, LOW);
       digitalWrite(CONTROL_PIN_2, HIGH);
     }
